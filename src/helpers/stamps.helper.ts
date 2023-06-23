c30f6d6244eb57653ae4e29534d6b10adf99c462
Herberton Candido Souza <herberton@gmail.com>
2023-06-23 02:20:28 -0300

    feat(airbyte): Add support to generate airbyte builder

--- src/helpers/stamps.helper.ts
+++ src/helpers/stamps.helper.ts
@@ -3,7 +3,8 @@ import { EnvironmentHelper } from "./environment.helper"
 export  type Stamps = {
     id: string,
     insert: string,
-    update: string
+    update: string,
+    limit: string
 }
 
 export class StampsHelper {
@@ -16,6 +17,7 @@ export class StampsHelper {
         stamps.id = stamps.id ?? EnvironmentHelper.get('DEFAULT_STAMP_ID', '_id')
         stamps.insert = stamps.insert ?? EnvironmentHelper.get('DEFAULT_STAMP_INSERT', 'createdAt')
         stamps.update = stamps.update ?? EnvironmentHelper.get('DEFAULT_STAMP_UPDATE', 'updatedAt')
+        stamps.limit = stamps.limit ?? EnvironmentHelper.get('DEFAULT_STAMP_LIMIT', '1000')
 
         return stamps
 
